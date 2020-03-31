3a9b7b13bf65c047e2475bfc167864e8ff06cd93
PatricioClark <patoclark@gmail.com>
2020-03-31 16:08:04 -0400

    Added equations, validation and activation

--- equations.py
+++ equations.py
@@ -871,3 +871,126 @@ def LES3DSmagWallDamping(model, coords, params):
           tau31_x + tau32_y + tau33_z)
 
     return [f0/sig_eq0, f1/sig_eq0, f2/sig_eq0, f3/sig_eq0]
+
+@tf.function
+def LES3DSmagWallDampingTerms(model, coords, params):
+    """ LES 3D equations with Smagorinsky model and Mason wall damping 
+        These equations re-normalize the inputs and outputs used from the NN """
+
+    PX    = params[0]
+    nu    = params[1]
+    delta = params[2]
+    c_s   = params[3]
+    U     = params[4]
+    V     = params[5]
+    W     = params[6]
+    sig_u = params[7]
+    sig_v = params[8]
+    sig_w = params[9]
+
+    sig_eq0 = 1.0
+    sig_eq1 = 1.0
+    sig_eq2 = 1.0
+    sig_eq3 = 1.0
+    if len(params)>13:
+        sig_eq0 = params[13]
+        sig_eq1 = params[14]
+        sig_eq2 = params[15]
+        sig_eq3 = params[16]
+
+    with tf.GradientTape(persistent=True) as tape2:
+        tape2.watch(coords)
+        with tf.GradientTape(persistent=True) as tape1:
+            tape1.watch(coords)
+            Yp = model(coords)[0]
+            u  = sig_u*Yp[:,0] + U
+            v  = sig_v*Yp[:,1] + V
+            w  = sig_w*Yp[:,2] + W
+            p  =       Yp[:,3]
+
+        # First derivatives
+        grad_u = tape1.gradient(u, coords)
+        u_t = grad_u[:,0]
+        u_x = grad_u[:,1]
+        u_y = grad_u[:,2]
+        u_z = grad_u[:,3]
+
+        grad_v = tape1.gradient(v, coords)
+        v_t = grad_v[:,0]
+        v_x = grad_v[:,1]
+        v_y = grad_v[:,2]
+        v_z = grad_v[:,3]
+
+        grad_w = tape1.gradient(w, coords)
+        w_t = grad_w[:,0]
+        w_x = grad_w[:,1]
+        w_y = grad_w[:,2]
+        w_z = grad_w[:,3]
+
+        grad_p = tape1.gradient(p, coords)
+        p_x = grad_p[:,1]
+        p_y = grad_p[:,2]
+        p_z = grad_p[:,3]
+
+        S11 = u_x
+        S12 = 0.5*(u_y+v_x)
+        S13 = 0.5*(u_z+w_x)
+        S22 = v_y
+        S23 = 0.5*(v_z+w_y)
+        S33 = w_z
+
+        coef = c_s*delta
+        eddy_viscosity = coef**2*tf.sqrt(2*(S11**2+2*S12**2+2*S13**2+
+                                                     S22**2+2*S23**2+
+                                                              S33**2))
+        tau11 = -2.0*eddy_viscosity*S11
+        tau12 = -2.0*eddy_viscosity*S12
+        tau13 = -2.0*eddy_viscosity*S13
+        tau22 = -2.0*eddy_viscosity*S22
+        tau23 = -2.0*eddy_viscosity*S23
+        tau33 = -2.0*eddy_viscosity*S33
+        del tape1
+
+    # Second derivatives
+    u_xx = tape2.gradient(u_x, coords)[:,1]
+    v_xx = tape2.gradient(v_x, coords)[:,1]
+    w_xx = tape2.gradient(w_x, coords)[:,1]
+
+    u_yy = tape2.gradient(u_y, coords)[:,2]
+    v_yy = tape2.gradient(v_y, coords)[:,2]
+    w_yy = tape2.gradient(w_y, coords)[:,2]
+
+    u_zz = tape2.gradient(u_z, coords)[:,3]
+    v_zz = tape2.gradient(v_z, coords)[:,3]
+    w_zz = tape2.gradient(w_z, coords)[:,3]
+    
+    tau11_x = tape2.gradient(tau11, coords)[:,1]
+    tau21_x = tape2.gradient(tau12, coords)[:,1]
+    tau31_x = tape2.gradient(tau13, coords)[:,1]
+
+    tau12_y = tape2.gradient(tau12, coords)[:,2]
+    tau22_y = tape2.gradient(tau22, coords)[:,2]
+    tau32_y = tape2.gradient(tau23, coords)[:,2]
+
+    tau13_z = tape2.gradient(tau13, coords)[:,3]
+    tau23_z = tape2.gradient(tau23, coords)[:,3]
+    tau33_z = tape2.gradient(tau33, coords)[:,3]
+    del tape2
+
+    # Output terms
+    return ([u_x, v_y, w_z],
+            [u_t,
+             u*u_x, v*u_y, w*u_z,
+             p_x, PX*tf.ones(p_x.shape, dtype=p_x.dtype),
+            -nu*u_xx, -nu*u_yy, -nu*u_zz,
+             tau11_x,  tau12_y,  tau13_z],
+            [v_t,
+             u*v_x, v*v_y, w*v_z,
+             p_y, 0*tf.ones(p_y.shape, dtype=p_y.dtype),
+            -nu*v_xx, -nu*v_yy, -nu*v_zz,
+             tau21_x,  tau22_y,  tau23_z],
+            [w_t,
+             u*w_x, v*w_y, w*w_z,
+             p_z, 0*tf.ones(p_z.shape, dtype=p_z.dtype),
+            -nu*w_xx, -nu*w_yy, -nu*w_zz,
+             tau31_x,  tau32_y,  tau33_z])
