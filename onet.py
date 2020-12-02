43b67bb961e54d9958e2100052a97f9a77214d04
Patricio Clark Di Leoni <patoclark@gmail.com>
2020-12-02 11:32:14 -0500

    Added tfrecord functions to onets

--- onet.py
+++ onet.py
@@ -375,3 +375,60 @@ class AdaptiveAct(keras.layers.Layer):
 
     def compute_output_shape(self, batch_input_shape):
         return batch_input_shape
+
+# #############
+# Datafunctions
+# #############
+
+def _float_feature(value):
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+# Used to generate the records
+def serialize_example(Xf, Xp, Y):
+    # After creating each array save it by doing:
+    # with tf.io.TFRecordWriter(f'data/{label}.tfrecord') as writer:
+    #     for ii in range(num):
+    #         serialized = serialize_example(combs_in[ii], p_in[ii], evol_out[ii])
+    #         writer.write(serialized)
+    feature = {
+        'Xf': _float_feature(Xf.flatten()),
+        'Xp': _float_feature(Xp.flatten()),
+        'Y':  _float_feature(Y.flatten()),
+    }
+    example    = tf.train.Example(features=tf.train.Features(feature=feature))
+    serialized = example.SerializeToString()
+
+    return serialized
+
+# Parse data
+def parse_proto(example_proto):
+    features = {
+        'Xf': tf.io.FixedLenFeature([800], tf.float32),
+        'Xp': tf.io.FixedLenFeature([2], tf.float32),
+        'Y':  tf.io.FixedLenFeature([], tf.float32),
+    }
+    parsed_features = tf.io.parse_single_example(example_proto, features)
+    return (parsed_features['Xf'], parsed_features['Xp']), parsed_features['Y']
+
+# Load dataset
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+def load_dataset(filepaths, batch_size, shuffle_buffer=0):
+    # Read records
+    dataset = tf.data.TFRecordDataset(filepaths)
+
+    # Disable order
+    ignore_order = tf.data.Options()
+    ignore_order.experimental_deterministic = False
+    dataset = dataset.with_options(ignore_order)
+
+    # Parse proto
+    dataset = dataset.map(parse_proto, num_parallel_calls=AUTOTUNE)
+
+    # Shuffle, prefetch and batch
+    if shuffle_buffer:
+        dataset = dataset.shuffle(shuffle_buffer)
+    dataset = dataset.prefetch(buffer_size=AUTOTUNE)
+    dataset = dataset.batch(batch_size)
+
+    return dataset
