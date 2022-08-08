c9d7f169b5cfdf7eec80a443eee53c150dc9e71a
PatricioClark <patoclark@gmail.com>
2022-08-08 10:21:53 -0300

    Refactoring

--- torchinn/net_archs.py
+++ torchinn/net_archs.py
@@ -2,37 +2,99 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import pytorch_lightning as pl
 
-import numpy as np
 
 # Basic MLP class
-# class MLP(nn.Module):
-#     def __init__(self, din, dout, depth, width, activation=nn.ELU, mask=None):
-#         """ Basic MLP
-# 
-#         mask : None or Tensor
-#             Tensor with mask
-#         """
+class MLP(nn.Module):
+    def __init__(self, din, dout, depth, width, activation=nn.ELU, mask=None):
+        """ Basic MLP """
+        super().__init__()
+
+        self.din  = din
+        self.dout = dout
+
+        net = []
+
+        # Mask
+        if mask is None:
+            self.mask = torch.ones(self.dout)
+        else:
+            self.mask = torch.tensor(mask)
+
+        # Input layer
+        net.append(nn.Linear(din, width))
+
+        # Hidden layers
+        for _ in range(depth):
+            net.append(nn.Linear(width, width))
+            net.append(activation())
+
+        # Output layer
+        net.append(nn.Linear(width, dout))
+
+        # Create model
+        self.model = nn.Sequential(*net)
+
+    def forward(self, x):
+        return self.model(self.mask*x)
+
+# class ActivatedLayer(nn.Module):
+#     def __init__(self, in_features, out_features,
+#                  activation, omega_0,
+#                  is_first=False, is_last=False):
 #         super().__init__()
-# 
+#         self.omega_0    = omega_0
+#         self.is_first   = is_first
+#         self.is_last    = is_last
+#         self.activation = activation
+#
+#         # Define activation function
+#         if self.activation == 'siren':
+#             self.act_fn = torch.sin
+#         elif self.activation == 'elu':
+#             self.act_fn = nn.functional.elu
+#
+#         if self.is_last:
+#             self.act_n = nn.Identity
+#
+#         self.in_features = in_features
+#         self.linear = nn.Linear(in_features, out_features)
+#
+#         if self.activation == 'siren':
+#             self.init_weights()
+#
+#     def init_weights(self):
+#         with torch.no_grad():
+#             if self.is_first:
+#                 self.linear.weight.uniform_(-1 / self.in_features, 
+#                                              1 / self.in_features)      
+#             else:
+#                 self.linear.weight.uniform_(-np.sqrt(6/self.in_features)/self.omega_0, 
+#                                              np.sqrt(6/self.in_features)/self.omega_0)
+#
+#     def forward(self, x):
+#         return self.act_fn(self.omega_0*self.linear(x))
+#
+# class MLP(nn.Module):
+#     def __init__(self, din, dout, depth, width,
+#                  activation='elu',
+#                  first_omega_0=1.0, hidden_omega_0=1.0):
+#         super().__init__()
+#    
 #         net = []
 # 
-#         # input layer
-#         net.append(nn.Linear(din, width))
+#         # Input layer
+#         net.append(ActivatedLayer(din, width, activation, first_omega_0, is_first=True))
 # 
-#         # hidden layers
+#         # Hidden layers
 #         for _ in range(depth):
-#             net.append(nn.Linear(width, width))
-#             net.append(activation())
+#             net.append(ActivatedLayer(width, width, activation, hidden_omega_0))
 # 
 #         # Output layer
-#         net.append(nn.Linear(width, dout))
-# 
+#         net.append(ActivatedLayer(width, dout, activation, hidden_omega_0, is_last=True))
+#         
 #         # Create model
 #         self.model = nn.Sequential(*net)
-# 
+#     
 #     def forward(self, x):
 #         return self.model(x)
-# 
--- torchinn/pinn.py
+++ torchinn/pinn.py
@@ -1,58 +1,73 @@
-# python3
+"""Physics Informed Neural Net Class
+
+Implemented with Pytorch Lightning
+"""
 
 import torch
-import torch.nn as nn
+from torch import nn
 import pytorch_lightning as pl
 
-import numpy as np
+from net_archs import MLP
+
 
 # PINN module
 class PhysicsInformedNN(pl.LightningModule):
-    def __init__(self, din, dout, depth, width,
-                 activation='elu', first_omega_0=1, hidden_omega_0=1,
-                 pde=None, eq_params=[], inv_ctes=[], inv_fields=[], lr=1e-3):
+    '''Physics Informed NN class'''
+    # pylint: disable=too-many-instance-attributes
+    # pylint: disable=too-many-ancestors
+
+    def __init__(self,
+                 nn_args,
+                 nn_kwargs=None,
+                 data_mask=None,
+                 base_nn='mlp',
+                 eq_params=None,
+                 inv_ctes=None,
+                 inv_fields=None,
+                 lr=1e-3):
+        # pylint: disable=too-many-arguments
         super().__init__()
 
-        self.din   = din
-        self.dout  = dout
-        self.depth = depth
-        self.width = width
-        self.activation     = activation
-        self.first_omega_0  = first_omega_0
-        self.hidden_omega_0 = hidden_omega_0
-
-        # Other params
+        # Params
+        self.nn_args = nn_args
+        self.base_nn = base_nn
         self.lr = lr
 
-        # Main network
-        self.main_net   = MLP(din, dout, depth, width,
-                              activation     = activation,
-                              first_omega_0  = first_omega_0,
-                              hidden_omega_0 = hidden_omega_0,
-                              )
+        if base_nn == 'mlp':
+            BaseNN = MLP
 
-        # PDE
-        if pde is None:
-            self.pde = lambda x,y,z: [torch.tensor([0.0])]
+        # Main network
+        if nn_kwargs is None:
+            nn_kwargs = {}
+        self.main_net = BaseNN(*nn_args, **nn_kwargs)
+
+        # Datamask
+        if data_mask is None:
+            self.data_mask = torch.ones(self.main_net.dout)
         else:
-            self.pde = pde
+            self.data_mask = torch.tensor(data_mask)
 
         # Eq params and inverse parts
         self.eq_params  = eq_params
-        cte_list        = [nn.Parameter(torch.tensor(k)) for k in inv_ctes]
-        self.inv_ctes   = nn.ParameterList(cte_list)
-        field_list      = [MLP(inv_in, inv_out, inv_w, inv_d,
-                               activation     = activation,
-                               first_omega_0  = first_omega_0,
-                               hidden_omega_0 = hidden_omega_0,
-                               )
-                           for inv_in, inv_out, inv_w, inv_d in inv_fields]
-        self.inv_fields = nn.ModuleList(field_list)
+        if inv_ctes is not None:
+            cte_list        = [nn.Parameter(torch.tensor(k)) for k in inv_ctes]
+            self.inv_ctes   = nn.ParameterList(cte_list)
+        if inv_fields is not None:
+            field_list      = [BaseNN(*inv_args, **inv_kwargs)
+                               for inv_args, inv_kwargs in inv_fields]
+            self.inv_fields = nn.ModuleList(field_list)
 
         # Save hyperparams
-        self.save_hyperparameters(ignore=['pde'])
+        self.save_hyperparameters()
+
+    def pde(self, z, x):
+        '''pde to enforce'''
+        # pylint: disable=unused-argument
+        return [torch.tensor([0.0])]
 
     def forward(self, x):
+        '''forward pass'''
+        # pylint: disable=arguments-differ
         out = self.main_net(x)
         return out
 
@@ -61,89 +76,27 @@ class PhysicsInformedNN(pl.LightningModule):
         return optimizer
 
     def training_step(self, batch, batch_idx):
+        '''forward pass'''
+        # pylint: disable=arguments-differ
+        # pylint: disable=unused-argument
         x, y = batch
         x = x.clone().detach().requires_grad_(True)
         z = self.main_net(x)
-        data_loss = nn.functional.mse_loss(y, z)
+        dy = self.data_mask*y
+        dz = self.data_mask*z
+        data_loss = nn.functional.mse_loss(dy, dz)
         self.log("data_loss", data_loss)
 
-        pde = self.pde(self, z, x)
+        pde = self.pde(z, x)
         phys_loss = torch.stack([eq**2 for eq in pde]).mean()
         self.log("phys_loss", phys_loss)
 
-        return data_loss + phys_loss
+        total_loss = data_loss + phys_loss
+        return total_loss
 
-class LinearLayer(nn.Module):
-    def __init__(self, in_features, out_features,
-                 activation, omega_0, is_first=False):
-        super().__init__()
-        self.omega_0  = omega_0
-        self.is_first = is_first
-        self.activation = activation
-        
-        self.in_features = in_features
-        self.linear = nn.Linear(in_features, out_features)
-        
-        self.init_weights()
-    
-    def init_weights(self):
-        if self.activation=='siren':
-            with torch.no_grad():
-                if self.is_first:
-                    self.linear.weight.uniform_(-1 / self.in_features, 
-                                                 1 / self.in_features)      
-                else:
-                    self.linear.weight.uniform_(-np.sqrt(6/self.in_features)/self.omega_0, 
-                                                 np.sqrt(6/self.in_features)/self.omega_0)
-            
-    def forward(self, x):
-        return self.linear(x)
-    
-class SirenAct(nn.Module):
-    """ Siren activation function """
-    def __init__(self, activation=torch.sin, omega0=30.0, **kwargs):
-        super().__init__(**kwargs)
-        self.activation = activation
-        self.omega0     = omega0
-
-    def forward(self, x):
-        out = self.activation(self.omega_0*x)
-        return out
-
-class MLP(nn.Module):
-    def __init__(self, din, dout, depth, width,
-                 activation='siren',
-                 first_omega_0=30.0, hidden_omega_0=30.0):
-        super().__init__()
-        
-        # Define activation function
-        if   activation=='siren': 
-            self.act_fn = SirenAct
-        elif activation=='elu':
-            self.act_fn = nn.ELU
-
-        net = []
-
-        # Input layer
-        net.append(LinearLayer(din, width, activation, first_omega_0, is_first=True))
-        net.append(self.act_fn())
-
-        # Hidden layers
-        for _ in range(depth):
-            net.append(LinearLayer(width, width, activation, hidden_omega_0))
-            net.append(self.act_fn())
-
-        # Output layer
-        net.append(LinearLayer(width, dout, activation, hidden_omega_0))
-        
-        # Create model
-        self.model = nn.Sequential(*net)
-    
-    def forward(self, x):
-        return self.model(x)
 
 def nn_grad(out, x):
-    ''' Returns gradient of out. One component at a time '''
+    ''' Returns gradient of out. Out must be a scalar '''
     return torch.autograd.grad(out, [x],
                                grad_outputs=torch.ones_like(out),
                                create_graph=True)[0]
